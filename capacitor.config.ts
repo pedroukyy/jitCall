50306dbda5e97a40e3c70235963f4374da360c16
pedroukyy <pedro.herrerabuelvas@unicolombo.edu.co>
2025-04-28 18:47:45 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'jitcall.videollamada',
   appName: 'jitCall',
   webDir: 'www'
 };
